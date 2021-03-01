77c483e995c87d79cdac287b673bc9c33946c4e3
Bonvallat <dan.bnvll@eduge.ch>
2021-03-01 13:28:04 +0100

    navBar

--- Ressources/php/navbar.inc.php
+++ Ressources/php/navbar.inc.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>NAVBAR</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-<link rel="stylesheet" href="Ressources/css/style.css">
-</head>
-<body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light" aria-current="true">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
