fdef6b7ec25ce30444d500692dadd7e7ec5696a3
Juliano Souza Luz <juliano.szlz@eduge.ch>
2021-03-01 15:57:21 +0100

    Update ajouter

--- ajouter.php
+++ ajouter.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<title>Menu</title>
+<title>Ajouter</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -9,15 +9,17 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 <link href="Ressources/css/style.css" rel="stylesheet">
 
-    <?php include_once("Ressources/php/navbar.inc.php");?>
+<?php include_once("Ressources/php/navbar.inc.php"); ?>
 
 <body class="w3-light-grey">
 
     <main>
-
+        <form method="POST" action="#">
+            <input type="submit" value="ajouter" name="ajouter"/>
+        </form>
     </main>
 
-    <?php include_once("Ressources/php/footer.inc.php")?>
+    <?php include_once("Ressources/php/footer.inc.php") ?>
 
 </body>
 
